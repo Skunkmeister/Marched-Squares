f33d2b7abab2c2a9a01b71d3f54d416109f54574
skunkmeister <c1a55yc0d3r@gmail.com>
2020-07-23 16:53:59 -0400

    Blurred heightfield implemented. Square marching algorithm remains

--- squaremarch.js
+++ squaremarch.js
@@ -5,7 +5,7 @@ let c = canvas.getContext('2d');
 console.log(canvas);
 
 let zLevel = 0;
-let girdScaleDivider = 10;
+let girdScaleDivider = 20;
 let xResolutionGLOBAL = canvas.width / (girdScaleDivider * 2);
 let yResolutionGLOBAL = canvas.height / (girdScaleDivider * 2);
 
@@ -14,7 +14,6 @@ let yResolutionGLOBAL = canvas.height / (girdScaleDivider * 2);
 function displayGrid(xResolution, yResolution, theArray) {
     for (var xi = 0; xi < xResolution; xi++) {
         for (var yi = 0; yi < yResolution; yi++) {
-            console.log(theArray[xi][yi]);
             drawCircle(xi * girdScaleDivider*2, yi * girdScaleDivider*2, 4, "rgb("+theArray[xi][yi]*255+","+theArray[xi][yi]*255+","+theArray[xi][yi]*255+")");
         }
     }
@@ -35,29 +34,36 @@ function drawCircle(centerX, centerY, radius = 5, strokeStyle = "rgb(99,0,0)") {
 }
 function randomArray(){
 
-    myLocalArray = makeArray(canvas.width, canvas.height);
-
-    
-    for(var x = 0; x < canvas.width; x++){
-        for(var y = 0; y < canvas.height; y++){
-            myLocalArray[x][y] = Math.random();
+    var myLocalArray = makeArray(canvas.height, canvas.width);
+    for(var row = 0; row < canvas.height; row++){
+        for(var column = 0; column < canvas.width; column++){
+            myLocalArray[row][column] = Math.random();
         }
     }
     return myLocalArray;
-
-
 }
-
-function makeArray(x,y){
-    let myArray = new Array(x)
-    for(var i = 0; i<y; i++){
-         myArray[i] = [];
+function makeArray(rows,columns){
+    let myArray = new Array(rows);
+    for(var i = 0; i<rows; i++){
+         myArray[i] = new Array(columns);
     }
     return myArray;
 }
-
-function processArray(){
+function processNoiseArray(myArray){   
+    myProcessedArray = makeArray(myArray.length-2, myArray[0].length-2)
     
+    console.log(myArray.length + "," + myArray[0].length)
+    console.log(myProcessedArray.length + "," + myProcessedArray[0].length)
+    for (var row = 1; row < myProcessedArray.length-1; row++) {
+        for (var column = 1; column < myProcessedArray[0].length-1; column++) {
+            myProcessedArray[row-1][column-1] = Math.round((myArray[row+1][column] + myArray[row-1][column] + myArray[row][column+1] + myArray[row][column-1] + myArray[row][column])/5);
+        }
+    }
+    console.log(myProcessedArray)
+    return myProcessedArray;
 }
 
-displayGrid(xResolutionGLOBAL, yResolutionGLOBAL, randomArray());
+
+let heightField = processNoiseArray(randomArray());
+displayGrid(xResolutionGLOBAL, yResolutionGLOBAL, heightField);
+
