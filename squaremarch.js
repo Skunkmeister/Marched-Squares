331b865752a9a6d00b3a4482386e0eda4ffa6d79
skunkmeister <c1a55yc0d3r@gmail.com>
2020-07-23 22:09:37 -0400

    Finished squaremarching visualization

--- squaremarch.js
+++ squaremarch.js
@@ -19,10 +19,10 @@ function displayGrid(xResolution, yResolution, theArray) {
         }
     }
 }
-function drawLine(x1, y1, x2, y2, lineWidth = 3, strokeStyle = "rgb(99,0,0)") {
+function drawLine(start, end, lineWidth = 3, strokeStyle = "rgb(99,0,0)") {
     c.beginPath();
-    c.moveTo(x1, y1);
-    c.lineTo(x2, y2);
+    c.moveTo(start.x, start.y);
+    c.lineTo(end.x, end.y);
     c.strokeStyle = strokeStyle;
     c.lineWidth = lineWidth;
     c.stroke();
@@ -36,6 +36,11 @@ function drawCircle(centerX, centerY, radius = 5, strokeStyle = "rgb(99,0,0)", i
         c.fill()
     }
     c.stroke();
+}
+function drawText(position, text){
+    c.beginPath
+    c.font = "12px Arial";
+    c.strokeText(text,position.x, position.y)
 }
 function randomArray(buffer) {
 
@@ -72,16 +77,18 @@ class vector2D {
 }
 function displaySquareMarch(myHeightField) {
     let canCoord = new vector2D();
+    let centerSquare = new vector2D();
     let pos1 = new vector2D();
     let pos2 = new vector2D();
     let pos3 = new vector2D();
     let pos4 = new vector2D();
-    for (let row = 0; row < myHeightField.length; row++) {
-        for (let column = 0; column < myHeightField.length; column++) {
+    for (let row = 0; row < myHeightField.length-1; row++) {
+        for (let column = 0; column < myHeightField[0].length-1; column++) {
             
-            console.log(myHeightField);
-            console.log(evaluateSquare(myHeightField, row, column));
-
+            canCoord.x = column*gridScaleDivider*2;
+            canCoord.y = row*gridScaleDivider*2;
+            centerSquare.x = canCoord.x + gridScaleDivider;
+            centerSquare.y = canCoord.y + gridScaleDivider;
             pos1.x = (canCoord.x + gridScaleDivider);
             pos1.y = (canCoord.y);
             pos2.x = (canCoord.x + 2*gridScaleDivider);
@@ -91,11 +98,51 @@ function displaySquareMarch(myHeightField) {
             pos4.x = (canCoord.x)
             pos4.y = (canCoord.y + gridScaleDivider);
 
+            switch(evaluateSquare(myHeightField, row, column)){
+                case 0:
+                case 15:
+                    break;
+                case 1:
+                case 14:
+                    drawLine(pos1, pos4);
+                    break;
+                case 2:
+                case 13:
+                    drawLine(pos1,pos2);
+                    break;
+                case 3:
+                case 12:
+                    drawLine(pos2,pos4);
+                    break;
+                case 4:
+                case 11:
+                    drawLine(pos2, pos3);
+                    break;
+                case 5:
+                    drawLine(pos1,pos2);
+                    drawLine(pos3,pos4);
+                    break;
+                case 6:
+                case 9:
+                    drawLine(pos1,pos3);
+                    break;
+                case 7:
+                case 8:
+                    drawLine(pos3,pos4);
+                    break;
+                case 10:
+                    drawLine(pos1,pos4);
+                    drawLine(pos2,pos3);
+                    break;
+
+
+            }
+
         }
     }
 }
 function evaluateSquare(theHeightField, row, column) {
-    return (theHeightfield[row, column] * 1 + theHeightfield[row, column + 1] * 2 + theHeightfield[row + 1, column + 1] * 4 + theHeightfield[row + 1, column] * 8);
+    return (theHeightField[row][column]*1 + theHeightField[row][column + 1] * 2 + theHeightField[row + 1][column + 1] * 4 + theHeightField[row + 1][column] * 8);
 
 }
 
@@ -108,5 +155,5 @@ myRandomArray = processNoiseArray(myRandomArray,1.4); //Returns an array 2 rows 
 console.log("The randomArray after processing: ")
 console.log(myRandomArray); 
 displayGrid(xResolutionGLOBAL, yResolutionGLOBAL, myRandomArray); //SUCCESS
-displaySquareMarch(heightField);
+displaySquareMarch(myRandomArray);
 
